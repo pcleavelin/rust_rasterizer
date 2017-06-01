962d757d06de338157eb90cec1bba277901388e1
Patrick Cleavelin <Patrick Cleavelin>
2017-05-31 19:28:21 -0700

    Added functions to convert 3d points into screen-space

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+#[macro_use] extern crate unborrow;
+#[macro_use] extern crate text_io;
+
 extern crate sdl2;
 use sdl2::*;
 use sdl2::rect::Point;
 use std::mem::swap;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::prelude;
 
 mod pointf;
-use pointf::Pointf;
+use pointf::*;
 
 fn create_sdl<'a>() -> (Sdl, VideoSubsystem, render::Renderer<'a>, EventPump) {
 	let sdl_context: Sdl;
@@ -56,17 +65,25 @@ fn create_sdl<'a>() -> (Sdl, VideoSubsystem, render::Renderer<'a>, EventPump) {
 
 #[derive(Copy, Clone)]
 pub struct Triangle {
-	v0: Pointf,
-	v1: Pointf,
-	v2: Pointf,
+	v0: Point2f,
+	v1: Point2f,
+	v2: Point2f,
 }
 
 impl Triangle {
-	pub fn new() -> Triangle {
+	pub fn new(v0: Point2f, v1: Point2f, v2: Point2f) -> Triangle {
 		Triangle {
-			v0: Pointf::new(1024.0/2.0, 1024.0/5.0),
-			v1: Pointf::new(1024.0/5.0, 1024.0-1024.0/5.0),
-			v2: Pointf::new(1024.0-1024.0/5.0, 1024.0-1024.0/5.0)
+			v0: v0,
+			v1: v1,
+			v2: v2,
+		}
+	}
+
+	pub fn standard() -> Triangle {
+		Triangle {
+			v0: Point2f::new(0.0,0.0),
+			v2: Point2f::new(0.0,1.0),
+			v1: Point2f::new(1.0,1.0),
 		}
 	}
 
@@ -78,15 +95,15 @@ impl Triangle {
 		}
 	}
 
-	pub fn v0(&self) -> Pointf {
+	pub fn v0(&self) -> Point2f {
         self.v0
     }
 
-	pub fn v1(&self) -> Pointf {
+	pub fn v1(&self) -> Point2f {
         self.v1
     }
 
-	pub fn v2(&self) -> Pointf {
+	pub fn v2(&self) -> Point2f {
         self.v2
     }
 
@@ -109,15 +126,15 @@ impl Triangle {
 	pub fn set_vert(&mut self, vert: usize, x: f32, y: f32) {
 		match vert {
 			0 => {
-				self.v0 = Pointf::from((x,y));
+				self.v0 = Point2f::from((x,y));
 			}
 
 			1 => {
-				self.v1 = Pointf::from((x,y));
+				self.v1 = Point2f::from((x,y));
 			}
 
 			2 => {
-				self.v2 = Pointf::from((x,y));
+				self.v2 = Point2f::from((x,y));
 			}
 			
 			_ => {
@@ -151,11 +168,11 @@ impl Triangle {
 fn fix_triangle(tri: &Triangle) -> (Triangle, Triangle) {
 	let tri = tri.sort();
 
-	let mut intersect = Pointf::new(tri.v1().x(), tri.v1().y());
-	let mut newxy = Pointf::new(tri.v1().x(), tri.v1().y());
+	let mut intersect = Point2f::new(tri.v1().x(), tri.v1().y());
+	let mut newxy = Point2f::new(tri.v1().x(), tri.v1().y());
 	let mut slope = tri.v0().slope(&tri.v2);
 	if tri.v1().y() > tri.v2().y() {
-		newxy = Pointf::new(tri.v2().x(), tri.v2().y());
+		newxy = Point2f::new(tri.v2().x(), tri.v2().y());
 		intersect = newxy;
 		slope = tri.v0().slope(&tri.v1);
 	}
@@ -166,12 +183,12 @@ fn fix_triangle(tri: &Triangle) -> (Triangle, Triangle) {
 		newxy.set_x((y - tri.v0().y())*slope + tri.v0().x());
 	}
 
-	let mut tri1 = Triangle::new();
+	let mut tri1 = Triangle::standard();
 	tri1.set_vert(0, tri.v0().x(), tri.v0().y());
 	tri1.set_vert(1, intersect.x(), intersect.y());
 	tri1.set_vert(2, newxy.x(), newxy.y());
 
-	let mut tri2 = Triangle::new();
+	let mut tri2 = Triangle::standard();
 	tri2.set_vert(0, tri.v1().x(), tri.v1().y());
 	tri2.set_vert(1, newxy.x(), newxy.y());
 	tri2.set_vert(2, tri.v2().x(), tri.v2().y());
@@ -217,21 +234,24 @@ fn draw_triangle_wireframe(tri: Triangle, xoffset: i32, yoffset: i32, renderer: 
 fn draw_triangle_solid(tri: Triangle, xoffset: i32, yoffset: i32, texture: &mut render::Texture) {
 	let sort_tri = tri.sort();
 
-	let mut slope_left = sort_tri.v0().slope(&sort_tri.v1());
-	let mut slope_right = sort_tri.v0().slope(&sort_tri.v2());
+	let mut slope_left = 0.0; //sort_tri.v0().slope(&sort_tri.v1());
+	let mut slope_right = 0.0; //sort_tri.v0().slope(&sort_tri.v2());
 	let mut top_left = sort_tri.v0().x();
 	let mut top_right = sort_tri.v0().x();
 
 	if sort_tri.v1().y() != sort_tri.v2().y() {
-
 		if sort_tri.v0().y() == sort_tri.v1().y() {
 			slope_left = sort_tri.v0().slope(&sort_tri.v2());
 			slope_right = sort_tri.v1().slope(&sort_tri.v2());
 			top_right = sort_tri.v1().x();
 		} else {
+			slope_left = sort_tri.v0().slope(&sort_tri.v1());
 			slope_right = sort_tri.v2().slope(&sort_tri.v1());
 			top_right = sort_tri.v2().x();
 		}
+	} else {
+		slope_left = sort_tri.v0().slope(&sort_tri.v1());
+		slope_right = sort_tri.v0().slope(&sort_tri.v2());
 	}
 
 	if top_left > top_right {
@@ -241,11 +261,16 @@ fn draw_triangle_solid(tri: Triangle, xoffset: i32, yoffset: i32, texture: &mut 
 		//top_right = sort_tri.v0().x();
 	}
 
-	let begin: i32 = sort_tri.v0().y() as i32;
-	let end: i32 = if sort_tri.v1().y() > sort_tri.v2().y() { sort_tri.v1().y() as i32 } else { sort_tri.v2().y() as i32 };
+	let mut begin: i32 = sort_tri.v0().y() as i32;
+	let mut end: i32 = if sort_tri.v1().y() > sort_tri.v2().y() { sort_tri.v1().y() as i32 } else { sort_tri.v2().y() as i32 };
+
+	if begin < 0 { begin = 0; }
+	if end >= 1024 { end = 1024-1; }
 
 	texture.with_lock(None, |pixels: &mut [u8], pitch: usize| {
 		for y in begin..end {
+
+			//if y%16 != 0 { continue; }
 
 			let mut begin_x: i32 = (((y-begin) as f32)*slope_left + top_left) as i32;
 			let mut end_x: i32 = (((y-begin) as f32)*slope_right + top_right) as i32;
@@ -253,6 +278,13 @@ fn draw_triangle_solid(tri: Triangle, xoffset: i32, yoffset: i32, texture: &mut 
 			if end_x < begin_x {
 				std::mem::swap(&mut begin_x, &mut end_x);
 			}
+
+			if begin_x >= 1024 || end_x < 0 {
+				continue;
+			}
+
+			if begin_x < 0 { begin_x = 0; }
+			if end_x >= 1024 { end_x = 1024-1; }
 
 			for x in begin_x..end_x {
 				let offset = (x*3) as usize + (y as usize) * pitch;
@@ -271,9 +303,40 @@ fn draw_triangle_solid(tri: Triangle, xoffset: i32, yoffset: i32, texture: &mut 
 }
 
 fn main() {
+	let path = Path::new("Maps/funtime.nmf");
+	let mut map_file = match File::open(&path) {
+		Ok(file) => file,
+		Err(why) => panic!("failed to open {}, {}", path.display(), why.description()),
+	};
+	let mut buf_reader = BufReader::new(map_file);
+	let mut contents = String::new();
+
 	let (mut sdl_context, mut sdl_video, mut renderer, mut event_pump) = create_sdl();
 
-	let mut tri1 = Triangle::new();
+	let mut triangles: Vec<Triangle> = Vec::new();
+	loop {
+		contents.clear();
+		buf_reader.read_line(&mut contents);
+
+		if contents.contains("vertex") {
+			println!("{}", contents);
+			let x: f32;
+			let z: f32;
+			scan!(contents.bytes() => "vertex {}, {}", x, z);
+		}
+
+		if contents.contains("neighbor") {
+			break;
+		}
+	}	
+
+	let mut counter: f32 = 0.0;
+	let mut v0 = Point3f::new( 0.0,  -0.50+(counter.sin()/2.0), 1.0);
+	let mut v1 = Point3f::new( 0.5,  -0.50+(counter.sin()/2.0), 1.0);
+	let mut v2 = Point3f::new( 0.75, -0.50+(counter.sin()/2.0), 5.0);
+
+	let mut tri1 = Triangle::new(v0.xy(), v1.xy(), v2.xy());
+
 	let mut current_vert = 0;
 	let mut is_moving_vert = false;
 
@@ -310,8 +373,9 @@ fn main() {
 
 		renderer.set_draw_color(sdl2::pixels::Color::RGB(0x00,0x00,0x00));
 		renderer.clear();
+		renderer.copy(&texture, None, None).unwrap();
 
-		texture.with_lock(None, |pixels: &mut [u8], pitch: usize| {
+		/*texture.with_lock(None, |pixels: &mut [u8], pitch: usize| {
 			for y in 0..1024 {
 				for x in 0..1024 {
 					let offset = (x*3) as usize + (y as usize) * pitch;
@@ -321,22 +385,32 @@ fn main() {
 					pixels[offset + 2] = 0;
 				}
 			}
-		}).unwrap();
+		}).unwrap();*/
 
+		counter += 0.1;
 		{
-			let (fix1, fix2) = fix_triangle(&mut tri1);
-			//draw_triangle_wireframe(tri1, 0, 0, &mut renderer);
-			//draw_triangle_wireframe(fix1, 0, 0, &mut renderer);
-			draw_triangle_wireframe(fix2, 16, 0, &mut renderer);
+			v0 = Point3f::new( 0.0+(counter.sin()*2.0), -0.5, 1.0);
+			v1 = Point3f::new( 0.5+(counter.sin()*2.0), 0.5, 1.0);
+			v2 = Point3f::new( 0.75+(counter.sin()*2.0), -0.5, 5.0);
 
-			draw_triangle_solid(fix1, 16, 0, &mut texture);
-			draw_triangle_solid(fix2, 16, 0, &mut texture);
+			v0 = v0.perspective_project();
+			v1 = v1.perspective_project();
+			v2 = v2.perspective_project();
+
+			tri1 = Triangle::new(v0.xy(), v1.xy(), v2.xy());
+
+			let (fix1, fix2) = fix_triangle(&mut tri1);
+			draw_triangle_wireframe(tri1, 0, 0, &mut renderer);
+			//draw_triangle_wireframe(fix1, 0, 0, &mut renderer);
+			//draw_triangle_wireframe(fix2, 16, 0, &mut renderer);
+
+			//draw_triangle_solid(fix1, 0, 0, &mut texture);
+			//draw_triangle_solid(fix2, 0, 0, &mut texture);
 		}
 		
-		renderer.copy(&texture, None, None).unwrap();
 		renderer.set_draw_color(sdl2::pixels::Color::RGB(0xc6,0x99,0x39));
 		let (x,y) = tri1.get_vert(current_vert);
-		renderer.draw_rect(sdl2::rect::Rect::new(x as i32 - 8,y as i32 - 8, 16, 16));
+		renderer.draw_rect(sdl2::rect::Rect::new(x as i32 - 8i32,y as i32 - 8i32, 16, 16));
 
 		renderer.present();
 	}
--- src/pointf.rs
+++ src/pointf.rs
@@ -1,93 +1,227 @@
 use std::ops::{Add, Neg, Div, Sub, Mul};
 
+
+/*
+A 2-dimensional vector
+*/
 #[derive(Copy, Clone, Debug)]
-pub struct Pointf {
-    x: f32,
-    y: f32
+pub struct Point2f {
+	x: f32,
+	y: f32
 }
 
-impl From<(f32, f32)> for Pointf {
-    fn from((x, y): (f32, f32)) -> Pointf {
-        Pointf::new(x, y)
-    }
+impl From<(f32, f32)> for Point2f {
+	fn from((x, y): (f32, f32)) -> Point2f {
+		Point2f::new(x, y)
+	}
 }
 
-impl Into<(f32, f32)> for Pointf {
-    fn into(self) -> (f32, f32) {
-        (self.x(), self.y())
-    }
+impl Into<(f32, f32)> for Point2f {
+	fn into(self) -> (f32, f32) {
+		(self.x, self.y)
+	}
 }
 
-impl Pointf {
-    /// Creates a new Pointf from the given coordinates.
-    pub fn new(x: f32, y: f32) -> Pointf {
-        Pointf {
-            x: x,
-            y: y,
-        }
-    }
+impl Point2f {
+	/// Creates a new Point2f from the given coordinates.
+	pub fn new(x: f32, y: f32) -> Point2f {
+		Point2f {
+			x: x,
+			y: y,
+		}
+	}
 
-    pub fn slope(&self, rhs: &Pointf) -> f32 {
-        (rhs.x() - self.x()) / (rhs.y() - self.y())
-    }
+	pub fn slope(&self, rhs: &Point2f) -> f32 {
+		(rhs.x - self.x) / (rhs.y - self.y)
+		//(rhs.y - self.y) / (rhs.x - self.x)
+	}
 
-    pub fn offset(&self, x: f32, y: f32) -> Pointf {
-        Pointf::new(self.x + x, self.y + y)
-    }
+	pub fn offset(&self, x: f32, y: f32) -> Point2f {
+		Point2f::new(self.x + x, self.y + y)
+	}
 
-    pub fn set_x(&mut self, x: f32) {
-        self.x = x;
-    }
+	pub fn set_x(&mut self, x: f32) {
+		self.x = x;
+	}
 
-    pub fn set_y(&mut self, y: f32) {
-        self.y = y;
-    }
+	pub fn set_y(&mut self, y: f32) {
+		self.y = y;
+	}
 
-    pub fn x(&self) -> f32 {
-        self.x
-    }
+	pub fn x(&self) -> f32 {
+		self.x
+	}
 
-    pub fn y(&self) -> f32 {
-        self.y
-    }
+	pub fn y(&self) -> f32 {
+		self.y
+	}
 }
 
-impl Add for Pointf {
-    type Output = Pointf;
+impl Add for Point2f {
+	type Output = Point2f;
 
-    fn add(self, rhs: Pointf) -> Pointf {
-        self.offset(rhs.x(), rhs.y())
-    }
+	fn add(self, rhs: Point2f) -> Point2f {
+		self.offset(rhs.x, rhs.y)
+	}
 }
 
-impl Neg for Pointf {
-    type Output = Pointf;
+impl Neg for Point2f {
+	type Output = Point2f;
 
-    fn neg(self) -> Pointf {
-        Pointf::new(-self.x(), -self.y())
-    }
+	fn neg(self) -> Point2f {
+		Point2f::new(-self.x, -self.y)
+	}
 }
 
-impl Sub for Pointf {
-    type Output = Pointf;
+impl Sub for Point2f {
+	type Output = Point2f;
 
-    fn sub(self, rhs: Pointf) -> Pointf {
-        self.offset(-rhs.x(), -rhs.y())
-    }
+	fn sub(self, rhs: Point2f) -> Point2f {
+		self.offset(-rhs.x, -rhs.y)
+	}
 }
 
-impl Mul<f32> for Pointf {
-    type Output = Pointf;
+impl Mul<f32> for Point2f {
+	type Output = Point2f;
 
-    fn mul(self, rhs: f32) -> Pointf {
-        Pointf::new(self.x * rhs, self.y * rhs)
-    }
+	fn mul(self, rhs: f32) -> Point2f {
+		Point2f::new(self.x * rhs, self.y * rhs)
+	}
 }
 
-impl Div<f32> for Pointf {
-    type Output = Pointf;
+impl Div<f32> for Point2f {
+	type Output = Point2f;
 
-    fn div(self, rhs: f32) -> Pointf {
-        Pointf::new(self.x() / rhs, self.y() / rhs)
-    }
+	fn div(self, rhs: f32) -> Point2f {
+		Point2f::new(self.x / rhs, self.y / rhs)
+	}
+}
+
+/*
+A 3-dimensional vector
+*/
+#[derive(Copy, Clone, Debug)]
+pub struct Point3f {
+	x: f32,
+	y: f32,
+	z: f32,
+}
+
+
+impl From<(f32, f32, f32)> for Point3f {
+	fn from((x, y, z): (f32, f32, f32)) -> Point3f {
+		Point3f::new(x, y, z)
+	}
+}
+
+impl Into<(f32, f32, f32)> for Point3f {
+	fn into(self) -> (f32, f32, f32) {
+		(self.x, self.y, self.z)
+	}
+}
+impl Point3f {
+	/// Creates a new Point3f from the given coordinates.
+	pub fn new(x: f32, y: f32, z: f32) -> Point3f {
+		Point3f {
+			x: x,
+			y: y,
+			z: z,
+		}
+	}
+
+	pub fn offset(&self, x: f32, y: f32, z: f32) -> Point3f {
+		Point3f::new(self.x + x, self.y + y, self.z + z)
+	}
+
+	pub fn to_camera_view(&self, cam_pos: Point3f, cam_rot: Point3f) -> Point3f{
+		let mut cam_view = Point3f::new(self.x - cam_pos.x(), self.y - cam_pos.y(), self.z - cam_pos.z());
+
+		let pcos = cam_rot.y().to_radians().cos();
+		let psin = cam_rot.y().to_radians().sin();
+
+		unborrow!(cam_view.set_x(pcos * cam_view.x() - psin * cam_view.z()));
+		unborrow!(cam_view.set_z(pcos * cam_view.z() + psin * cam_view.x()));
+
+		cam_view
+	}
+
+	pub fn perspective_project(&self) -> Point3f {
+		let mut e = Point3f::new((1024.0 / 2.0) * self.x / self.z,
+								(1024.0 / 2.0) * self.y / self.z,
+								1.0 / (0.73f32 * 1024.0 / 2.0).tan());
+
+		let mut b = Point3f::new(1024.0 / 2.0 + ((e.z() / self.z) * self.x - e.x()),
+								1024.0 / 2.0 + ((e.z() / self.z) * self.y - e.y()),
+								self.z);
+
+		b
+	}
+
+	pub fn xy(&self) -> Point2f {
+		Point2f::new(self.x, self.y())
+	}
+
+	pub fn set_x(&mut self, x: f32) {
+		self.x = x;
+	}
+
+	pub fn set_y(&mut self, y: f32) {
+		self.y = y;
+	}
+
+	pub fn set_z(&mut self, z: f32) {
+		self.z = z;
+	}
+
+	pub fn x(&self) -> f32 {
+		self.x
+	}
+
+	pub fn y(&self) -> f32 {
+		self.y
+	}
+
+	pub fn z(&self) -> f32 {
+		self.z
+	}
+}
+
+impl Add for Point3f {
+	type Output = Point3f;
+
+	fn add(self, rhs: Point3f) -> Point3f {
+		self.offset(rhs.x, rhs.y, rhs.z)
+	}
+}
+
+impl Neg for Point3f {
+	type Output = Point3f;
+
+	fn neg(self) -> Point3f {
+		Point3f::new(-self.x, -self.y, -self.z)
+	}
+}
+
+impl Sub for Point3f {
+	type Output = Point3f;
+
+	fn sub(self, rhs: Point3f) -> Point3f {
+		self.offset(-rhs.x, -rhs.y, -rhs.z)
+	}
+}
+
+impl Mul<f32> for Point3f {
+	type Output = Point3f;
+
+	fn mul(self, rhs: f32) -> Point3f {
+		Point3f::new(self.x * rhs, self.y * rhs, self.z * rhs)
+	}
+}
+
+impl Div<f32> for Point3f {
+	type Output = Point3f;
+
+	fn div(self, rhs: f32) -> Point3f {
+		Point3f::new(self.x / rhs, self.y / rhs, self.z / rhs)
+	}
 }
